623e55ab1b6bc50616be05951abf6fdfdde16129
MianHannan <hannanchudri@gmail.com>
2020-11-26 16:53:49 +0500

    CICD test3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export const App = () => {
   return (
     <div>
       <h1>Covid19 Tracker</h1>
-      <h2>Surge Deploy CICD test number 2</h2>
+      <h2>Surge Deploy CICD test number 3</h2>
     </div>
   )
 }
